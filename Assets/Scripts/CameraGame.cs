f341b549a77d1685296c1d943a8dc8b65bd99139
Milena Crivella <milenaverissimo@gmail.com>
2015-10-11 17:25:24 -0300

    Stage 1 - The Witch
    Puzzle 1

---
+++ Assets/Scripts/CameraGame.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraGame : MonoBehaviour {
+
+    void CameraMovement()
+    {
+        Camera.main.transform.position = new Vector2(Input.mousePosition.x, this.transform.position.y);
+    }
+
+    void Update()
+    {
+        CameraMovement();
+    }
+}
---
+++ Assets/Scripts/CatchingObjects.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CatchingObjects : MonoBehaviour {
+
+    void OnMouseDrag()
+    {
+        Vector2 position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        this.transform.position = position;
+    }
+
+    void OnMouseUp()
+    {
+        this.transform.position = new Vector2(7.5f, -4);
+    }
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.name.Equals("Cobweb"))
+        {
+            Destroy(coll.gameObject);
+        }
+    }
+}
