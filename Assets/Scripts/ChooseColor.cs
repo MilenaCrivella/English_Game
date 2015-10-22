14e87fe543fd785c6fe8c8fefa07169d9bab841c
Fabieli Helena <fabielihelena@gmail.com>
2015-10-21 22:27:02 -0200

    Stage 1 - The Witch
    Puzzle 2 - test

--- Assets/Scripts/ChooseColor.cs
+++ Assets/Scripts/ChooseColor.cs
@@ -1,10 +1,18 @@
 ﻿using UnityEngine;
+using System;
 using System.Collections;
+using System.Collections.Generic;
 
 public class ChooseColor : MonoBehaviour {
 
+    public GameObject Color; 
+
 	void OnMouseDown()
 	{
-
+        if (this.gameObject.name == "Blue")
+        {
+            Debug.Log("Choose");
+            Color.SetActive(false);
+        }
 	}
 }
--- Assets/Scripts/PaintWall.cs
+++ Assets/Scripts/PaintWall.cs
@@ -5,15 +5,23 @@ using System.Collections.Generic;
 
 public class PaintWall : MonoBehaviour {
 
-	public GameObject ChooseColor2; 
+	public GameObject ChooseColor2;
+    public GameObject color1; 
+
 
 	void OnMouseDown()
 	{
-		if (this.gameObject.name == "Brush") ChooseColor2.SetActive (true);
-		if (this.gameObject.name == "Blue") 
+        if (this.gameObject.name == "Brush")
+        {
+            ChooseColor2.SetActive(true);
+            color1.SetActive(true);
+
+        }
+		if (this.gameObject.name == "bluee") 
 		{
 			Debug.Log ("ooi");
-			ChooseColor2.SetActive (false);
+			//ChooseColor2.SetActive (false);
+            //color1.SetActive(false);
 		}
 	}
 }
