23ef58600b3197c44731aba65995f91133cdfb3f
Fabieli Helena <fabielihelena@gmail.com>
2015-11-06 09:52:51 -0200

    Stage 1 - The Witch
    Move Camera

---
+++ Assets/Scripts/MoveCamera.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveCamera : MonoBehaviour {
+
+	private bool isMov = false;
+
+    void OnMouseDown()
+    { 
+		Debug.Log ("OI, MILENA!");
+		isMov = true;
+    }
+
+	void Update() {
+		if (isMov) {
+			Camera.main.transform.position = new Vector3(
+				Mathf.Lerp(Camera.main.transform.position.x, 20f, 5f * Time.deltaTime),
+				Camera.main.transform.position.y, Camera.main.transform.position.z);
+		}
+	}
+}
